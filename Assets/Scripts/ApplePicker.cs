f9a6ed3737470abec446b8fc8eb86ca7c2a59897
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2020-09-14 22:54:41 -0400

    baskets now

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 public class ApplePicker : MonoBehaviour
 {
     // Start is called before the first frame update
+    [Header("set in inspector")]
+    public GameObject basketPrefab;
+    public int numberOfBaskets = 3;
+    public float basketBottom = -14f;
+    public float basketSpacing = 2f;
+
     void Start()
     {
-        
+        for(int i=0; i<numberOfBaskets; i++)
+        {
+            GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottom +( basketSpacing*i);
+            tBasketGO.transform.position = pos;
+
+        }
     }
 
     // Update is called once per frame
