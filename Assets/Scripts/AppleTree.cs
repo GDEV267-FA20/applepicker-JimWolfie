c17d531b9d25c3fb337a2f46afd1eac4e4004846
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2020-09-14 21:48:48 -0400

    step1 of script

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+    [Header("Prefab Settings")]
+    public GameObject applePrefab;
+    public float speed=1f;
+    //how fast the tree moves
+    public float radius=10f;
+    //distance the tree moves across the screen
+    public float directionChangeChance =0.1f;
+    //how often tree changes direction
+    public float dropSpeed= 1f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //dropping apples every second
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //basic movement
+        //change diretction 
+
     }
 }
