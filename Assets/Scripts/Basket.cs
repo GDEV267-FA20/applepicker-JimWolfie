b5a62d6f4f16a0ebab7ae3a328414a18c22577e9
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2020-09-16 09:41:08 -0400

    part 1

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Basket : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        InputActionAsset controls = GetComponent<InputActionAsset>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject collidedWith = collision.gameObject;
+        if(collidedWith.tag == "Apple")
+        {
+            Destroy(collidedWith);
+        }
     }
 }
