bad3e52b721fd4642f0ec77ba2870d3db152b85f
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2020-09-16 22:55:18 -0400

    got mouse working.

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -5,16 +5,37 @@ using UnityEngine.InputSystem;
 
 public class Basket : MonoBehaviour
 {
+    BasketMove controls;
+    private void Awake()
+    {
+        controls = new BasketMove();
+
+    }
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        InputActionAsset controls = GetComponent<InputActionAsset>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 mousePos2D = controls.Mouse.Position.ReadValue<Vector2>();
         
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+
     }
     private void OnCollisionEnter(Collision collision)
     {
